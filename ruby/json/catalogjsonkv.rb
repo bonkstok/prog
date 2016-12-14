6b05903fadffb3c1475d9c733caea90c8be9bfc7
bonkstok <johnnyboythabest@hotmail.com>
2016-12-14 13:40:03 +0100

    Create catalogjsonkv.rb

---
+++ ruby/json/catalogjsonkv.rb
@@ -0,0 +1,20 @@
+require 'json'
+print "enter file name to write to:"
+write_file = File.new(gets.chomp + ".json", "w")
+
+print "\n Enter name of json file: "
+json_file = File.read(gets.chomp + ".json")
+
+data_hash = JSON.parse(json_file)
+#puts data_hash.keys
+
+i = 1
+data_hash.each do |key, value|
+	write_file.puts("#{i} -- Key: #{key} --\n Value: #{value}")
+	i += 1
+end
+
+#data_hash["tags"].each { |tag| puts tag}
+#puts data_hash["tags"][0]
+write_file.close()
+#json_file.close()
