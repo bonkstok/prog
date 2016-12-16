23ad1dbcdd86026451d5bd45c9c2e8fe2fd45caa
bonkstok <johnnyboythabest@hotmail.com>
2016-12-16 14:37:25 +0100

    Create find_arr_or_hsh.rb

---
+++ ruby/find_arr_or_hsh.rb
@@ -0,0 +1,19 @@
+require 'json'
+#file_write = File.new('read_parameter', 'w')
+hsh = {empty: true}
+#hsh.each {|key,value| file_write.puts("key =---> #{key} =---> #{value}")}
+#hsh['install_switches'].each {|key, value| puts key}
+#puts hsh['install_switches'].class
+json_file = File.read('sw444v1105.json')
+data_hash = JSON.parse(json_file)
+hash_length = data_hash['resources'].length
+i = 0
+
+for i in i...hash_length
+	if data_hash['resources'][i].class == Hash
+		puts (i+1) == hash_length  ? "HASH at last index data_hash['resources'] index #{i}" : "HASH at data_hash['resources'] index #{i}"
+	elsif data_hash['resources'][i].class == Array
+		puts "ARRAY at data_hash['resources'] index #{i}"
+	else
+		puts "some weird class dude." end 
+end 
