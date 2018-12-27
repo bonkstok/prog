a3cfdbd280ddfeb3dcb8ce502f79420ab0b3f57f
bonkstok <johnnyboythabest@hotmail.com>
2018-12-27 11:51:45 +0100

    Update gmaps.py

--- python/jsondata/gmaps.py
+++ python/jsondata/gmaps.py
@@ -5,7 +5,7 @@ bestemming = input('Adres:')
 bestemming = bestemming.split(',')
 print(bestemming)
 
-#url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Oostervenne+239+Purmerend&destinations=rijksdorp+wassenaar&mode=driving&key=AIzaSyC6lUl5i5cDy3ryvoblgoLOhsovHAP7-Ls'
+#url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Oostervenne+239+Purmerend&destinations=rijksdorp+wassenaar&mode=driving&key=-Ls'
 url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins={}+{}+{}&destinations=maastricht+Nederland&mode={}&key=-Ls'.format(bestemming[0],bestemming[1],bestemming[2],mode)
 response = urlopen(url)
 print(url)
